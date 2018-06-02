7f85f486ef468bf8625bf92c14e98240484e1ea4
MrFive <MrFive9999@qq.com>
2018-06-02 13:35:27 +0800

    算法：寻找坏的芯片

---
+++ AlgorithmExercise/Ramdom.cpp
@@ -0,0 +1,14 @@
+#include "Random.h"
+
+#include <cstdlib>
+#include <ctime>
+
+bool Random::initRandom = false;
+
+int Random::rand() {
+    if (!initRandom) {
+        srand((unsigned)time(NULL));
+        initRandom = true;
+    }
+    return ::rand();
+}
